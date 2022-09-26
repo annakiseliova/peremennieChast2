8b8232d0895859ad295f2a83f92dc224ffedfd0e
Анна <annakiseliova7@gmail.com>
2022-09-26 15:58:49 +0300

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 18888;
+        byte b = 5;
+        short c = 588;
+        long d = 58975998L;
+        float e = 889f;
+        double f = 86111;
+        System.out.println("Значение переменной a c типом int равно " + a);
+        System.out.println("Значение переменной b c типом byte равно " + b);
+        System.out.println("Значение переменной c c типом short равно " + c);
+        System.out.println("Значение переменной d c типом long равно " + d);
+        System.out.println("Значение переменной e c типом float равно " + e);
+        System.out.println("Значение переменной f c типом double равно " + f);
+    }
+}
