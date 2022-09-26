64dc09642f4d70e4997fc78233b1e1ea8968c1c5
Анна <annakiseliova7@gmail.com>
2022-09-26 16:44:23 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-       byte classLudmilaPavlovna = 23;
-       byte classAnnaSergeevna = 27;
-       byte classEkaterinaAndreevna = 30;
-       short allSheetsOfPaper = 480;
-       int sheetOfPaperPerOneStudent = allSheetsOfPaper/(classLudmilaPavlovna + classAnnaSergeevna + classEkaterinaAndreevna);
-        System.out.println("На каждого ученика рассчитано " + sheetOfPaperPerOneStudent + " листов бумаги");
+        int machinePerfomance = 16;
+        short time = 2;
+        int machinePerfomanceInOneMinute = machinePerfomance / time;
+        machinePerfomance = machinePerfomanceInOneMinute * 20;
+        System.out.println("За 20 минут машина произвела бутылок " + machinePerfomance +" штук");
+        machinePerfomance = machinePerfomanceInOneMinute * 24 * 60;
+        System.out.println("За сутки машина произвела бутылок " + machinePerfomance +" штук");
+        machinePerfomance = machinePerfomanceInOneMinute * 24 * 60 * 3;
+        System.out.println("За 3 дня машина произвела бутылок " + machinePerfomance +" штук");
+        machinePerfomance = machinePerfomanceInOneMinute * 24 * 60 * 30;
+        System.out.println("За месяц машина произвела бутылок " + machinePerfomance +" штук");
     }
 }
+
+
+
